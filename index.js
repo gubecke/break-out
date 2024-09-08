1668105d6b4d1b1d5e3e862c673ed60918f44959
Guillaume Becker <guillaume.becker2013@gmail.com>
2024-09-08 11:11:02 +0200

    start blocks configuration

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const block = document.createElement('div')
+const grid = document.querySelector('.grid')
+const blockWidth = 100
+const blockHeight = 20
+class Block {
+    constructor(xAxis, yAxis) {
+        this.bottomLeft = [xAxis,yAxis]
+        this.bottomRight = [xAxis + blockWidth, yAxis]
+        this.topLeft = [xAxis, yAxis + blockHeight]
+        this.topRhight = [xAxis + blockWidth, yAxis + blockHeight]
+    }
+}
+const blocks = [
+    new Block(10,270)
+]
+function addBlock() {
+    const block = document.createElement('div')
+    block.classList.add('block')
+    block.style.left = '100px'
+    block.style.bottom = '50px'
+    grid.appendChild(block)
+}
+addBlock()
